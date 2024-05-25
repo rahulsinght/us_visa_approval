31717874121844c547c04da303e1a7f20a15bdb2
rahul <rahulsinghtagur@gmail.com>
2024-05-25 09:07:01 +0530

    setup file changed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'us_visa', 
+    version='0.0',
+    description='us-visa-approval-mlops-project',
+    author='rahul',
+    author_email='rahulsinghtagur@gmail.com',
+    packages=find_packages()
+    )
+
